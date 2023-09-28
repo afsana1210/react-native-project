c533344d8eaac183fdc9e9613c0c4588179a12d6
afsana1210 <afsanaansari1210@gmail.com>
2023-09-28 19:51:06 +0530

    modified test file added

--- components/Test.js
+++ components/Test.js
@@ -2,15 +2,19 @@ import { Text, View } from "react-native";
 
 export default function Test(){
     return(
-        // <View
-        // style={{
-        //     flexDirection:"row",
-        //     height:200,
-        //     padding:20
-        // }}>
-        //     <View style={{backgroundColor:"blue" , flex: 0.5}}></View>
-        //     <View style={{backgroundColor:"red" , flex: 0.3}}></View>
-        // </View>
+        <>
+        <View
+        style={{
+            flexDirection:"row",
+            height:200,
+            padding:20
+        }}>
+            <View style={{backgroundColor:"blue" , flex: 0.5}}></View>
+            <View style={{backgroundColor:"red" , flex: 0.3}}></View>
+        </View>
         <Text style={{fontWeight:"bold"}}>I am bold<Text style={{color:"red"}}>and red </Text></Text>
+
+        </>
+       
     )
 }
