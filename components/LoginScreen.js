ddf7cefea1d2b138f178df28358c9ddc620f7c85
afsana1210 <afsanaansari1210@gmail.com>
2023-10-03 19:23:56 +0530

    Modified login screen file added

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -41,7 +41,7 @@ const style=StyleSheet.create({
         fontSize:24,
         padding:20,
         marginVertical:9,
-        color:"EDEFEE",
+        color:"#EDEFEE",
         textAlign:'center'
     },
     input:{
@@ -50,9 +50,10 @@ const style=StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         fontSize: 16,
-        borderColor: 'EDEFEE',
+        borderColor: '#EDEFEE',
         backgroundColor: 'white',
         color:"EDEFEE",
+        textAlign:"center"
     }
 });
 
