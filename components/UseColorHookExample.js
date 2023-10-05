6858c9df365c3aa635b7997aeab377196d88c093
afsana1210 <afsanaansari1210@gmail.com>
2023-10-05 16:44:36 +0530

    React navtive Hooks Example file added

---
+++ components/UseColorHookExample.js
@@ -0,0 +1,39 @@
+import { Image, ScrollView, StyleSheet, Text, useColorScheme } from "react-native"
+
+const UseColorHookExample=()=>{
+    const colorScheme = useColorScheme();
+    return(
+        <ScrollView style={[
+            style.container,
+            colorScheme === 'light' 
+            ? {backgroundColor:"#fff"}
+            : {backgroundColor : "#333333"}]}>
+                {''}
+            <Image
+            style={style.logo}
+              source={require("../Img/LittleLemonLogo.PNG")}
+              resizeMode="center"
+              accessible={true}
+              accessibilityLabel="Logo"
+            />
+            <Text style={style.regularText}>Color Scheme : {colorScheme}</Text>{''}
+        </ScrollView>
+    )
+}
+const style=StyleSheet.create({
+    container:{
+        flex:1,
+        padding:24,
+        marginTop:25
+    },
+    logo:{
+        height:100,
+        width:300
+    },
+    regularText:{
+        textAlign:"center",
+        fontSize:18
+    }
+})
+
+export default UseColorHookExample;
