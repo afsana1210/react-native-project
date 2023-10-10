affd611ab68590a72eec42662f736b6b75233c12
afsana1210 <afsanaansari1210@gmail.com>
2023-10-10 21:00:52 +0530

    Pressable component added to welcomeScree to navigate

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,27 +1,30 @@
-import { View,Text, ScrollView } from "react-native";
+import { View,Text, ScrollView ,Pressable, StyleSheet} from "react-native";
 
 
-export default function WelcomeScreen(){
+export default function WelcomeScreen({navigation}){
 return (
     <>
-    {/* <View style={{flex:0.75}}> */}
         <ScrollView 
         indicatorStyle={"white"}
         style={{
-            //  paddingHorizontal: 40,
-            //  paddingVertical: 40,
              backgroundColor: "black",
             flex: 1
         }}>
         <Text style={{padding:40,fontSize:60,color:"white",textAlign:"center"}}>Welcome to Little Lemon</Text>
-        <Text style={{padding:40, fontSize:40,color:"white",textAlign:"center",marginVertical:8}}>Little lemon is a charming neighborhood bistro the serves simple food and classic cocktail in a lively dfkldsjflkjfkljdskfdklf fndknfkdnf dnfalsknfmdnvj jfnjdnv mdnvejwfnvcc jfncnjfe kncdm lwjef jcndkhfliefnmv  jdnvkldjfiejnv dnckvdjligjndnv mdnckldjfiejnv dmvnldshfilehjnv mvndljfoiej dmnvjkdhfoeh dmnckjehfoihe cnlekfihe</Text>
+        <Text style={{padding:40, fontSize:40,color:"white",textAlign:"center",marginVertical:8}}>Little lemon is a charming neighborhood bistro the serves simple food and classic cocktail in a lively </Text>
 
-        {/* <Text>some text <Text>Again Text</Text></Text> */}
-        {/* <View>some text....</View> */}
+        <Pressable onPress={() => navigation.navigate('Menu')}> 
+       <Text style={styles.buttonText}>View Menu</Text> 
+       </Pressable> 
         </ScrollView>
-        
-       
-    {/* </View> */}
     </>
 )
 }
+
+const styles=StyleSheet.create({
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 25,
+      },
+})
