13f0781df73a5db86ebbeea6cc1042d4c1eafbf7
afsana1210 <afsanaansari1210@gmail.com>
2023-10-10 21:04:33 +0530

    React Navigation added to App.js file

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import WelcomeScreen from './components/WelcomeScreen';
-import MenuItem from './components/MenuItem';
-import Test from './components/Test';
+import SectionListExample from './components/SectionListExample';
+import LoginScreenWithPresableComp from './components/LoginScreenWithPressableComp';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Test/> */}
-      <Header />
-      <WelcomeScreen/>
-      {/* <MenuItem /> */}
-      <Footer/>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+          <Header />
+      <Stack.Navigator initialRouteName='Welcome' >
+      <Stack.Screen
+       options={{title:'Home'}}
+       name='Welcome'
+       component={WelcomeScreen}/>
+      <Stack.Screen name='Login' component={LoginScreenWithPresableComp}/>
+      <Stack.Screen name='Menu' component={SectionListExample}/>
+      </Stack.Navigator>
+      </View>
+      <View style={styles.footerContainer}>
+          <Footer/>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#495E57',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#333333',
+  },
+  footerContainer:{
+    backgroundColor:'#333333'
   }
-  
 });
